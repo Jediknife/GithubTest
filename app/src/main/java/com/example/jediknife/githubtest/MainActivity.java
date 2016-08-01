1e745ac4d10a1d211b4a6231d268ae42d7ac6e81
Jediknife 100 <Jediknife 100>
2016-07-31 20:42:11 -0700

    Initial commit

---
+++ app/src/main/java/com/example/jediknife/githubtest/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.jediknife.githubtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView HelloWorld = findViewById(R.id.helloWorldTextView);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
